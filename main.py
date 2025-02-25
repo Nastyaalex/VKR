2b81eb674c0916ae86f60d375eeb5062dac77d24
nastyaalex09@outlook.com <nastyaalex09@outlook.com>
2025-02-25 23:09:30 +0300

    пример не удовлетворительный

--- main.py
+++ main.py
@@ -8,15 +8,37 @@ app = Flask(__name__)
 def hello():
     message = ""
     if request.method == "POST":
-        area = request.form.get("area")
+        area1 = request.form.get("area1")
+        area2 = request.form.get("area2")
+        area3 = request.form.get("area3")
+        area4 = request.form.get("area4")
+        area5 = request.form.get("area5")
+        area6 = request.form.get("area6")
+        area7 = request.form.get("area7")
+        area8 = request.form.get("area8")
+        area9 = request.form.get("area9")
+        area10 = request.form.get("area10")
+        area11 = request.form.get("area11")
+        area12 = request.form.get("area12")
         try:
-            area = float(area)
+            area1 = float(area1)
+            area2 = float(area2)
+            area3 = float(area3)
+            area4 = float(area4)
+            area5 = float(area5)
+            area6 = float(area6)
+            area7 = float(area7)
+            area8 = float(area8)
+            area9 = float(area9)
+            area10 = int(area10)
+            area11 = float(area11)
+            area12 = float(area12)
         except Exception as e:
             print(e)
-            message += "Некорректный ввод. Установлено значение по умолчанию: 0 кв.м."
-            area = 0.0
-        cost = get_prediction(area)
-        message += f"Стоимость квартиры площадью {area} кв.м. равна {cost} рублей."
+            message += "Некорректный ввод."
+
+        cost = get_prediction(area1,area2,area3,area4,area5,area6,area7,area8,area9,area10,area11,area12)
+        message += f"Прочность при растяжении, МПа равна {cost} ."
     return render_template("index.html", message=message)
 
 if __name__ == '__main__':
--- process.py
+++ process.py
@@ -1,6 +1,12 @@
-def get_prediction(area):
-    # TODO: Добавить загрузку модели из файла
-    # Примечание: если использовалась предобработка, то нужно тоже ее выполнить в коде приложения
-    # TODO: Добавить получение предсказаний загруженной модели
-    cost = 300_000 * area + 120_000
-    return cost
+import pickle
+import sklearn
+import numpy as np
+
+def get_prediction(area1,area2,area3,area4,area5,area6,area7,area8,area9,area10,area11,area12):
+    with open('lr_model.pkl', 'rb') as f:
+        loaded_model = pickle.load(f)
+
+    data = [area8,area1,area2,area3,area4,area5,area6,area7,area9,area10,area11,area12]
+
+    y_pred = loaded_model.predict(np.array(data).reshape(1, -1).astype(float))
+    return y_pred
