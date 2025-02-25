c2cead5179495ec750f431270e85266b3dba535e
nastyaalex09@outlook.com <nastyaalex09@outlook.com>
2025-02-25 21:53:24 +0300

    пример

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+from process import get_prediction
+
+app = Flask(__name__)
+
+@app.route('/', methods=["get", "post"])
+def hello():
+    message = ""
+    if request.method == "POST":
+        area = request.form.get("area")
+        try:
+            area = float(area)
+        except Exception as e:
+            print(e)
+            message += "Некорректный ввод. Установлено значение по умолчанию: 0 кв.м."
+            area = 0.0
+        cost = get_prediction(area)
+        message += f"Стоимость квартиры площадью {area} кв.м. равна {cost} рублей."
+    return render_template("index.html", message=message)
+
+if __name__ == '__main__':
+    app.run()
